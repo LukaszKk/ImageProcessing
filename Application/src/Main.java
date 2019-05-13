06c4ae2ee550e50af36355f800474e8aa5b2b9b1
Łukasz Kuk <lukasz0104201@wp.pl>
2019-05-13 17:50:06 +0200

    creating library

--- Application/src/Main.java
+++ Application/src/Main.java
@@ -2,49 +2,48 @@ import image.processing.ImageProcess;
 import image.processing.ImageTransform;
 import image.processing.Reconstruction;
 
-
 import java.awt.image.BufferedImage;
 
-public class Main {
-
-    public static void main(String[] args)
+public class Main
+{
+    public static void main( String[] args )
     {
         ImageTransform im = new ImageTransform();
         Reconstruction re = new Reconstruction();
-        BufferedImage background = ImageProcess.bufferedImage( "src\\images\\background.png" );
-        BufferedImage image = ImageProcess.bufferedImage( "src\\images\\image.png" );
+        BufferedImage background = ImageProcess.bufferedImage("src\\images\\background.png");
+        BufferedImage image = ImageProcess.bufferedImage("src\\images\\image.png");
 
         try
         {
-
             BufferedImage image1 = im.toGray(background);
-            ImageProcess.saveImage( image1, "src\\images\\result\\image1" );        //background to gray
+            ImageProcess.saveImage(image1, "src\\images\\result\\image1");        //background to gray
 
             BufferedImage image2 = im.toGray(image);
-            ImageProcess.saveImage( image2, "src\\images\\result\\image2" );        //image to gray
+            ImageProcess.saveImage(image2, "src\\images\\result\\image2");        //image to gray
 
             BufferedImage image3 = im.differentiation(image1, image2);
-            ImageProcess.saveImage( image3, "src\\images\\result\\image3" );        //differentation
+            ImageProcess.saveImage(image3, "src\\images\\result\\image3");        //differentation
 
             BufferedImage image4 = im.binarize(image3, 40);
-            ImageProcess.saveImage( image4, "src\\images\\result\\image4" );        //binarization
+            ImageProcess.saveImage(image4, "src\\images\\result\\image4");        //binarization
 
             BufferedImage image5 = im.closing(image4, 5);
-            ImageProcess.saveImage( image5, "src\\images\\result\\image5" );        //closing
+            ImageProcess.saveImage(image5, "src\\images\\result\\image5");        //closing
 
             BufferedImage image6 = im.opening(image5, 3);
-            ImageProcess.saveImage( image6, "src\\images\\result\\image6" );        //opening
+            ImageProcess.saveImage(image6, "src\\images\\result\\image6");        //opening
 
             BufferedImage image7 = im.erosion(image6, 10);
-            ImageProcess.saveImage( image7, "src\\images\\result\\image7" );        //erosion
+            ImageProcess.saveImage(image7, "src\\images\\result\\image7");        //erosion
 
             BufferedImage image8 = re.reconstruct(image7, image6);
-            ImageProcess.saveImage( image8, "src\\images\\result\\image8" );        //reconstruction
+            ImageProcess.saveImage(image8, "src\\images\\result\\image8");        //reconstruction
 
-        }
-        catch(Exception e)
+            ImageProcess.showImage("src\\images\\result\\image8");
+
+        } catch( Exception e )
         {
-            System.out.println("exception");
+            e.printStackTrace();
         }
     }
 }
